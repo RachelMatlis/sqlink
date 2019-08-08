1e9aa0a81710fc6791769cd8e221034a768e45e9
RachelMatlis <Rachel20008@gmail.com>
2019-08-08 13:15:58 +0300

    Update strings.c

--- C/strings.c
+++ C/strings.c
@@ -3,26 +3,37 @@
 
 void myStrcpy(char*, char* );
 void myStrcat(char* , char* );
-void myStrcmp();
+int myStrcmp(char*, char*);
 
 void printString(char *str);
 
-
-
-
 int main()
 {
-	char s1[20]= "Rachel 455 ";
+	char s1[20]= "Rachel";
 	char s2[20] = "Matlis";
+	char s3[20]= "Rachel";
+	char s4[20] = "Matlis";
+	char s5[20]= "yoni";
+	char s6[20] = "yosi";
+	int strcmpRes;
 	
+		
+	printf("Strcpy(%s, %s): ", s1,s2);
 	myStrcpy(s1, s2);
+		
+	printf("\nStrcat(%s, %s): ", s3,s4);
+	
+	printf("\nStrcmp(%s, %s): ", s5,s6);
+	strcmpRes = myStrcmp(s5, s6);
+	printf("%d", strcmpRes);
+	
+	
 }
 
 /*Copies the string pointed by s1 into the array pointed by s2, including the terminating null character (and stopping at that point).*/
 void myStrcpy(char * s1, char *s2)
 {
 	int i=0;
-	
 	for(i; i< strlen(s2) ; i++)
 	{
 		s1[i] = s2[i]; 
@@ -42,7 +53,42 @@ void myStrcpy(char * s1, char *s2)
 /*Appends a copy of the source string to the destination string. The terminating null character in destination is overwritten by the first character of source, and a null-character is included at the end of the new string formed by the concatenation of both in destination.*/
 void myStrcat(char* s1, char* s2 )
 {
+	int i =0;
+	int EndOfS1 = (strlen(s1))+1;
+
+	for(i= EndOfS1; i< strlen(s2); i++)
+	{
+		s1[i] = s2[i];
+	}
 	
+	printString(s1);
+	
+}
+
+/*Compares the C string str1 to the C string str2.
+This function starts comparing the first character of each string. If they are equal to each other, it continues with the following pairs until the characters differ or until a terminating null-character is reached.*/
+int myStrcmp(char* s1, char* s2 )
+{
+	int i=0;
+    for (i; strlen(s1); i++)
+    {
+        if (s1[i] != s2[i])
+        {
+        	if(s1[i] < s2[i])
+        	{
+        		return -1;
+        	}
+        	else
+        	{
+        		return 1;
+        	}
+        }
+
+        if (s1[i] == '\0' && s2[i] == '\0')
+        {
+            return 0;
+        }
+    }
 }
 
 void printString(char* str)
@@ -54,7 +100,6 @@ void printString(char* str)
 		printf("%c", str[i]);
 	}
 }
-
 
 
 
