cac24409e133e0d6d2dc542dbbeadde91b654082
RachelMatlis <Rachel20008@gmail.com>
2019-08-11 18:02:17 +0300

    Add files via upload

---
+++ C/insert/functions.c
@@ -0,0 +1,60 @@
+/* In general it's good to include also the header of the current .c,
+   to avoid repeating the prototypes */
+#include "functions.h"
+
+DA* createDA(int capacity)
+{
+	DA* da = (DA*)malloc(sizeof(DA));
+	
+	if(da)
+    {
+        da->arr = (int *)malloc(capacity* sizeof(int));
+        da->index = 0;
+        da-> capacity = capacity;
+        return da;
+    }
+}
+
+void destroyDA(DA* da)
+{
+	if(da)
+	{
+		free(da->arr);
+		free(da);
+	}
+}
+
+int insert(DA* da, int num)
+{
+	if(da->index < da->capacity)
+	{
+		da->arr[(da->index)] = num;
+		(da->index)++;
+	}
+	else
+	{
+		da->arr = (int*)realloc(da->arr,((da->capacity) *2) *sizeof(int));
+		da->arr[da->index] = num;
+		(da->index)++;
+		(da->capacity)*=2;
+	}
+	
+	return 1;
+}
+
+void printArr(DA* da)
+{
+	int i = 0;
+	for (i; i<(da->index) - 1; i++)
+	{
+		printf("%d, ", da->arr[i]);
+	}
+
+	printf("%d.", da->arr[da->index-1]);
+	
+}
+
+
+
+
+
---
+++ C/insert/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<stdlib.h>
+/* To use the functions defined in Functions.c I need to #include Functions.h */
+#include "functions.h"
+
+int main(void)
+{
+	int capacity=0, num =0, iteration =0;
+	DA* da = NULL;
+
+	printf("Enter array capacity: \n");
+	scanf("%d", &capacity);
+	
+	da = createDA(capacity);
+	
+	if(da)
+	{
+		while(iteration < capacity+1)
+		{
+			printf("insert number: \n");
+			scanf("%d", &num);
+			insert(da, num);
+			iteration++;
+		}
+	}
+	
+	printArr(da);
+	
+	destroyDA(da);
+    return 0;
+}
