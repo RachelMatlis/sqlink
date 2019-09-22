92a67e08c6e3133a7f47f4758a24b5fe5ac249a8
RachelMatlis <Rachel20008@gmail.com>
2019-09-22 21:31:30 +0300

    Add files via upload

---
+++ C++/01_string_t/string_t.h
@@ -0,0 +1,23 @@
+#ifndef STRING_T_H
+#define STRING_T_H
+
+class String_t
+{
+	public:
+		String_t(); /*default ctor*/
+		String_t(const char* ); /*ctor*/
+		String_t(const String_t& ); /*copy ctor*/
+		~String_t(); /*d'ctor*/
+		String_t& operator=(const String_t&); /*assigment operator*/
+		int length() const;
+		void setString(const char*);
+		char* getString() const;
+		int compare(const String_t&);
+		void print() const;
+	private:
+		char* str;
+		int size; 
+};
+
+#endif
+
---
+++ C++/01_string_t/string_t_main.cpp
@@ -0,0 +1,51 @@
+# include <iostream>
+# include <string.h>
+#include "string_t.h"
+
+using namespace std;
+
+int main()
+{   
+	int choice;
+	char str[] = "";
+	char newStr[] = "";
+
+	cout << "Enter String:";
+	cin >> str;
+
+	String_t st(str);
+
+	cout << endl<<"Choose option:" << endl;
+	do 
+	{
+		cout << endl;
+		cout << "1.Set string" << endl;
+		cout << "2.Get string" << endl;
+		cout << "3.Get string length" << endl;
+		cout << "4.Print string" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			cout << "Enter String: ";
+			cin >> newStr;
+			st.setString(newStr);
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			cout << endl<<"The string: ";
+			st.print();
+			break;
+		default:
+			break;
+		}
+
+
+	} while (choice != 5);
+
+}
+
+
