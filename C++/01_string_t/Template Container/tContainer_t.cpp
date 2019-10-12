1f6000688f5329101f94e801eb35fe33cb0f587d
RachelMatlis <Rachel20008@gmail.com>
2019-10-12 15:18:11 +0300

    Add files via upload

--- C++/01_string_t/Template Container/tContainer_t.cpp
+++ C++/01_string_t/Template Container/tContainer_t.cpp
@@ -2,8 +2,7 @@
 #include<iostream> 
 
 
-void vectorContainerTest();
-void ListContainerTest();
+void showMenue();
 
 int main()
 {
@@ -11,17 +10,9 @@ int main()
 	tContainer_t<int, vector<int*>> container;
 	int choice = 0;
 
-	while (choice != 10)
+	while (choice != 12)
 	{
-		cout << endl<<"Choose option:" << endl;
-		cout << "1. Insert new element" << endl;
-		cout << "2. Is container empty" << endl;
-		cout << "3. Number of elements in container" << endl;
-		cout << "4. First element in container" << endl;
-		cout << "5. Last element in container" << endl;
-		cout << "6. Find specific element" << endl;
-		cout << "7. Remove specific element" << endl;
-		cout << "8. Remove all the elements" << endl;
+		showMenue();
 		cin >> choice;
 
 		switch (choice)
@@ -43,7 +34,7 @@ int main()
 		case 4:
 			if (!container.isEmpty())
 			{
-				cout << "--> The first elemnt is: " << *(container.getFirst()) << endl;
+				cout << "--> The first elemnt is: " << *container.getFirst() << endl;
 			}
 			else
 			{
@@ -61,19 +52,53 @@ int main()
 			}
 			break;
 		case 6:
-			cout << "Enter elemnt to to find: " << endl;
-			cin >> x;
-			cout << "--> " << x;
-			container.find(x) != 0 ?  cout << " was found." : cout << " was not found.";
+			if (!container.isEmpty())
+			{
+				cout << "Enter elemnt to to find: " << endl;
+				cin >> x;
+				cout << "--> " << x;
+				container.find(x) != 0 ? cout << " was found." : cout << " was not found.";
+			}
+			else
+			{
+				cout << "--> The container is empty.";
+			}
 			cout << endl;
 			break;
 		case 7:
+			if (!container.isEmpty())
+			{
+				cout << "Enter elemnt to to remove: " << endl;
+				cin >> x;
+				if (container.remove(x)!=0)
+				{
+					cout << "--> " << *container.remove(x) << "has removed"<<endl;
+				}
+				else
+				{
+					cout << "Elemnt not found: " << endl;
+				}
+			}
+			else
+			{
+				cout << "--> The container is empty.";
+			}
 			break;
 		case 8:
 			container.removeAll();
 			cout << "--> All elemnts has removed." << endl;
 			break;
+		case 9:
+			break;
 		case 10:
+			break;
+		case 11:
+			cout << "Enter index to get value " << endl;
+			cin >> x;
+			cout <<"-->  container[" <<x << "] = "<<*(container[x]) << endl;
+			break;
+
+		case 12:
 			cout << "GOODBYE!" << endl;
 			break;
 		default:
@@ -86,3 +111,19 @@ int main()
 	return 0;
 }
 
+void showMenue()
+{
+	cout << endl << "Choose option:" << endl;
+	cout << "1. Insert new element" << endl;
+	cout << "2. Is container empty" << endl;
+	cout << "3. Number of elements in container" << endl;
+	cout << "4. First element in container" << endl;
+	cout << "5. Last element in container" << endl;
+	cout << "6. Find specific element" << endl;
+	cout << "7. Remove specific element" << endl;
+	cout << "8. Remove all the elements" << endl;
+	cout << "9. Remove and delete elemnt" << endl;
+	cout << "10. Remove and delete all elemnts" << endl;
+	cout << "11. Get value by index ([])" << endl;
+	cout << "12. End program" << endl;
+}
--- C++/01_string_t/Template Container/tContainer_t.h
+++ C++/01_string_t/Template Container/tContainer_t.h
@@ -5,6 +5,7 @@
 #include <typeinfo>
 #include <vector>
 #include <list>
+#include<iostream>
 #include"Equal_t.h"
 
 using namespace std;
@@ -24,12 +25,12 @@ public:
 	T* getLast() const;
 
 	T* find(const T& value) const;
-	//T* remove(T* element);
-
+	T* remove(const T& value);
 	void removeAll();
-	//void removeAndDelete(T* element);
-	//void removeAndDeleteAll();
-	//T& operator[](const tContainer_t&);
+
+	void removeAndDelete(const T& element);
+	void removeAndDeleteAll();
+	T* operator[](const unsigned int index);
 
 private:
 	/*not allow copy*/
@@ -46,9 +47,10 @@ private:
 template<class T, class C>
 T* tContainer_t<T, C>::getFirst() const
 { 
+	cout << *m_container.front();
 	if (!isEmpty())
 	{
-		return *(m_container.begin());
+		return m_container.front();
 	}
 
 	return 0;
@@ -79,4 +81,69 @@ void tContainer_t<T, C>::removeAll()
 	{
 		m_container.clear();
 	}
+}
+
+template<class T, class C>
+T* tContainer_t<T, C>::remove(const T& value)
+{
+	iter_t itr = find_if(m_container.begin(), m_container.end(), Equal_t<T>::Equal_t(value));
+	T* elemntP;
+
+	if (itr == m_container.end()) /*value not found*/
+	{
+		return 0;
+	}
+	else
+	{
+		elemntP = *itr;
+		m_container.erase(itr);
+	}
+
+	return elemntP;
+}
+
+template<class T, class C>
+void tContainer_t<T, C>::removeAndDelete(const T& element)
+{
+	iter_t itr = find_if(m_container.begin(), m_container.end(), Equal_t<T>::Equal_t(value));
+	if (itr != m_container.end()) /*value not found*/
+	{
+		m_container.erase(itr);
+		delete *itr;
+	}
+}
+
+template<class T, class C>
+void tContainer_t<T, C>::removeAndDeleteAll()
+{
+	while (m_container.begin()++ != m_container.end())
+	{
+		m_container.erase(m_container.begin());
+		delete m_container.begin();
+	}
+}
+
+template<class T, class C>
+T* tContainer_t<T, C>::operator[](const unsigned int index)
+{
+	if (m_container.size() > index)
+	{
+		if (typeid(C) == typeid(vector<T*>))
+		{
+			return (*(vector<T*>*)& m_container)[index];
+		}
+		else if (typeid(C*) == typeid(list<T*>*))
+		{
+			iterConst_t itr = m_container.begin();
+			for (int i = 0; i < index; i++)
+			{
+				itr++;
+			}
+
+			return *itr;
+		}
+	}
+
+	return 0;
+	
 }
