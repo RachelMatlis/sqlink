3548f34430b5e25b4a5074fa080afec7d5f37a31
RachelMatlis <Rachel20008@gmail.com>
2019-10-23 21:06:38 +0300

    Add files via upload

--- C++/01_string_t/input_output/input_output_test.cpp
+++ C++/01_string_t/input_output/input_output_test.cpp
@@ -5,45 +5,176 @@
 
 using namespace std;
 
-//num Status { ok_e, cant_open_file_e, bad_access_e, writeErr_e, readErr_es };
+typedef enum{ Ascii =1, Binary=2, Else =3} FileType;
+
+void testIO(FileType fType);
+void showOpenError(virtIO_t::Status error);
+void showMenue();
+void showStatus(virtIO_t::Status status);
+
+void read(virtIO_t& virtIO);
+void write(virtIO_t& virtIO);
+
 int main()
 {
-	string path = "/home/sqrt17/Desktop/C++/input_output/test.txt";
-	string mode = "w";
-	int i=100;
-	int j=10;
+	FileType fType = Else;
+	int choice;
 
-	//virtIO_t* ascii = new asciiIO_t();
-
-	virtIO_t* binIO = new binIO_t();
-
-	try
+	while (fType != Ascii && fType != Binary)
 	{
-		binIO->open(path , mode);
-
-		//ascii->open(path , mode);
-		//*ascii<<i;//cout
-
-		//*ascii>>j;//cin
-		//cout<<endl<<j<<endl;
-
-
-	}
-
-	catch(virtIO_t::Status error)
-	{
-		switch(error)
+		cout << "Choose test:" << endl;
+		cout << "1. Ascii" << endl;
+		cout << "2. Binary" << endl;
+		cin >> choice;
+		if (choice == 1)
 		{
-			case 1:
-				cout<<"cant_open_file_e"<<endl;
-				break;
-			case 2:
-				cout<<"bad_access_e"<<endl;
-				break;
-			default:
-				break;
+			fType = Ascii;
+		}
+		else if (choice == 2)
+		{
+			fType = Binary;
 		}
 	}
 
-	return 0;
+	testIO(fType);
+
+}
+
+void testIO(FileType fType)
+{
+	virtIO_t* virtIO = 0;
+	string path, mode;
+
+	cout << endl << "Enter path: ";
+	cin >> path;
+	cout << "Enter mode: ";
+	cin >> mode;
+
+	switch (fType)
+	{
+	case Ascii:
+		try 
+		{
+			virtIO = new asciiIO_t(path.c_str(), mode.c_str());
+		}
+		catch (virtIO_t::Status error)
+		{
+			showOpenError(error);
+			return;
+		}
+		break;
+	case Binary:
+		try
+		{
+			virtIO = new binIO_t(path.c_str(), mode.c_str());
+		}
+		catch (virtIO_t::Status error)
+		{
+			showOpenError(error);
+			return;
+		}
+		break;
+	default:
+		break;
+	}
+
+	int option=0;
+	size_t pos;
+	while (option !=9)
+	{
+		showMenue();
+		cin >> option;
+		switch (option)
+		{
+		case 1:
+			cout << "---> Path: " << virtIO->getPath()<<endl;
+			break;
+		case 2:
+			cout << "---> Access: " << virtIO->getAccess() << endl;
+			break;
+		case 3:
+			showStatus(virtIO->getStatus());
+			break;
+		case 4:
+			cout << "---> Length: " << virtIO->getLength() << endl;
+			break;
+		case 5:
+			//cout << "---> Position: " << virtIO->getPosition() << endl;
+			//break;
+		case 6:
+			cout << "Enter position: ";
+			cin >> pos;
+			virtIO->setPosition(pos);
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			cout << "GoodBye!"<<endl;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void showOpenError(virtIO_t::Status error)
+{
+	switch (error)
+	{
+	case 1:
+		cout << "Cant open file" << endl;
+		break;
+	case 2:
+		cout << "Bad access " << endl;
+		break;
+	default:
+		break;
+	}
+}
+
+void showMenue()
+{
+	cout << endl;
+	cout << "Choose option:" << endl;
+	cout << "1. Get path" << endl;
+	cout << "2. Get access" << endl;
+	cout << "3. Get status" << endl;
+	cout << "4. Get length" << endl;
+	cout << "5. Get position" << endl;
+	cout << "6. Set position" << endl;
+	cout << "7. Read from file ( >> )" << endl;
+	cout << "8. Write to file ( << )" << endl;	
+}
+
+void showStatus(virtIO_t::Status status)
+{
+	cout << "---> Status: ";
+	switch (status)
+	{
+	case 0:
+		cout << "ok";
+		break;
+	case 1:
+		cout << "cant open file";
+		break;
+	case 2:
+		cout << " bad access";
+		break;
+	case 3:
+		cout << "write Err";
+		break;
+	case 4:
+		cout << "read Err";
+		break;
+	default:
+		break;
+	}
+	cout << endl;
+}
+
+void write(virtIO_t& virtIO)
+{
+
 }
--- C++/01_string_t/input_output/virtIO_t.cpp
+++ C++/01_string_t/input_output/virtIO_t.cpp
@@ -12,7 +12,7 @@ virtIO_t::virtIO_t()
 virtIO_t::virtIO_t(const string& i_path, const string& i_access)
 {
 	open(i_path, i_access);
-} 
+}
 
 virtIO_t::~virtIO_t()
 {
@@ -21,14 +21,14 @@ virtIO_t::~virtIO_t()
 
 void virtIO_t::open(const string& i_path, const string& i_access)
 {
-	if (i_access!= "r" && i_access != "w" && i_access != "r+" && i_access != "w+")
+	if (i_access != "r" && i_access != "w" && i_access != "r+" && i_access != "w+")
 	{
 		throw bad_access_e;
 	}
 
 	m_fileP = fopen(i_path.c_str(), i_access.c_str());
 
-	if(m_fileP)
+	if (m_fileP)
 	{
 		m_path = i_path;
 		m_access = i_access;
@@ -42,7 +42,7 @@ void virtIO_t::open(const string& i_path, const string& i_access)
 
 void virtIO_t::close()
 {
-	if(m_fileP)
+	if (m_fileP)
 	{
 		close();
 	}
@@ -50,34 +50,34 @@ void virtIO_t::close()
 
 size_t virtIO_t::getLength() const
 {
-	if(m_fileP) 
+	if (m_fileP)
 	{
-		  fseek(m_fileP, 0 , SEEK_END); //move file pointer to a specific position
-		  size_t fileLenght = ftell(m_fileP); //position of pointer
-		  fseek(m_fileP, m_position, SEEK_SET);// needed for next read from last position of file
+		size_t currPos = ftell(m_fileP);//position of pointer
+		fseek(m_fileP, 0, SEEK_END); //move file pointer to a specific position
+		size_t fileLenght = ftell(m_fileP); 
+		fseek(m_fileP, currPos, SEEK_SET);// needed for next read from last position of file
 
-		  return fileLenght;
+		return fileLenght;
 	}
 
 	return 0;
 }
 
-void virtIO_t::setPosition(const size_t& position)
+bool virtIO_t::setPosition(const size_t& position)
 {
 	if (m_fileP && position <= getLength())
 	{
 		fseek(m_fileP, position, SEEK_SET);
 		m_position = position;
+		return true;
 	}
+	return false;
 }
 
 inline size_t virtIO_t::getPosition() const
 {
-	return ftell(m_fileP);
+	if (m_fileP)
+	{
+		return ftell(m_fileP);
+	}
 }
-
-
-
-
-
-
