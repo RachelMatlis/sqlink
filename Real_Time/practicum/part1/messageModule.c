dc6783ed22d9fae666b4e2a1a4832ca01a0dcd57
RachelMatlis <Rachel20008@gmail.com>
2019-11-24 17:08:39 +0200

    Add files via upload

--- Real_Time/practicum/part1/messageModule.c
+++ Real_Time/practicum/part1/messageModule.c
@@ -28,8 +28,6 @@ static const struct file_operations queue_fops = {
 static struct class *my_class;
 /* this variable will hold our cdev struct */
 static struct cdev cdev;
-/* this variable will store the device */
-static struct device *my_device;
 /* this is the first dev_t allocated to us... */
 static dev_t first_dev;
 /* number of minors we need... */
@@ -38,74 +36,112 @@ static int mq_count = 8;
 static int first_minor;
 
 
-/*struct my_mq_t {
+struct my_mq_t {
 	size_t size;
 	size_t capacity;
 	struct list_head list;
 	struct device *mq_device; 
-};*/
+};
+
+static struct my_mq_t* m_queues; 
+
+static inline void m_queues_ctor(struct my_mq_t* m_queues)
+{
+	m_queues->size =0;
+	m_queues->capacity=100;
+	INIT_LIST_HEAD(&(m_queues->list));
+	m_queues->mq_device = NULL;
+}
+
+static inline void m_queues_dtor(const struct my_mq_t *m_queue)
+{
+
+}
 
 static int __init mq_init(void)
 {
-	int err;
-	pr_info("init start\n");
+	int ret;
+	int i;
+	/* allocate all queues */
+	m_queues = kmalloc(sizeof(struct my_mq_t)*mq_count, GFP_KERNEL);
+	if (IS_ERR(m_queues)) 
+	{
+		pr_err("kmalloc\n");
+		ret = PTR_ERR(m_queues);
+		goto err_return;
+	}
+	/* initialize all m_queues */
+	for (i = 0; i < mq_count; i++)
+		m_queues_ctor(m_queues+i);
 	/* allocate our own range of devices */
-	err = alloc_chrdev_region(&first_dev, first_minor, mq_count,THIS_MODULE->name);
-	if (err) 
+	ret = alloc_chrdev_region(&first_dev, first_minor, mq_count,THIS_MODULE->name);
+	if (ret) 
 	{
 		pr_err("cannot alloc_chrdev_region\n");
-		goto err_nothing;
+		goto err_final;
 	}
-	pr_info("allocated the region\n");
+	pr_debug("allocated the region\n");
 	/* add the cdev structure */
 	cdev_init(&cdev, &queue_fops);
-	err = cdev_add(&cdev, first_dev, mq_count);
-	if (err) 
+	ret = cdev_add(&cdev, first_dev, mq_count);
+	if (ret) 
 	{
 		pr_err("cannot cdev_add\n");
-		goto err_cdev_del;
+		goto err_dealloc;
 	}
-	pr_info("added the cdev\n");
-	/* this is creating a new class (/sys/class) */
+	pr_debug("added the cdev\n");
+	/* this is creating a new class (/proc/devices) */
 	my_class = class_create(THIS_MODULE, THIS_MODULE->name);
 	if (IS_ERR(my_class)) 
 	{
-		pr_err("failed to create class\n");
-		err = PTR_ERR(my_class);
+		pr_err("class_create\n");
+		ret = PTR_ERR(my_class);
 		goto err_cdev_del;
 	}
-	pr_info("created class\n");
-	/* and now lets auto-create a /dev/ node */
-	my_device = device_create(my_class, NULL, first_dev,"%s", THIS_MODULE->name);
-	if (IS_ERR(my_device)) 
+	pr_debug("created the class\n");
+	for (i = 0; i < mq_count; i++) 
 	{
-		pr_err("failed to create device\n");
-		err = PTR_ERR(my_device);
-		goto err_class;
+		/* and now lets auto-create a /dev/ node */
+		m_queues[i].mq_device = device_create(my_class, NULL,
+			MKDEV(MAJOR(first_dev), MINOR(first_dev)+i),
+			NULL, "%s%d", THIS_MODULE->name, i);
+		if (IS_ERR(m_queues[i].mq_device)) 
+		{
+			pr_err("device_create\n");
+			ret = PTR_ERR(m_queues[i].mq_device);
+			goto err_class;
+		}
 	}
-	pr_info("emitted message to udev to create /dev file in user space\n");
-	pr_info("device loaded successfuly\n");
+	pr_info(KBUILD_MODNAME " loaded successfully\n");
 	return 0;
-/* err_device:
-	device_destroy(my_class, first_dev);
-*/
+
 err_class:
 	class_destroy(my_class);
 err_cdev_del:
 	cdev_del(&cdev);
+err_dealloc:
 	unregister_chrdev_region(first_dev, mq_count);
-err_nothing:
-	return err;
+err_final:
+	for (i = 0; i < mq_count; i++)
+		m_queues_dtor(m_queues+i);
+	kfree(m_queues);
+err_return:
+	return ret;
 }
 
 static void __exit mq_exit(void)
 {
-	pr_info("exit start\n");
-	device_destroy(my_class, first_dev);
+	int i;
+	for (i = 0; i < mq_count; i++)
+		device_destroy(my_class, MKDEV(MAJOR(first_dev),
+			MINOR(first_dev)+i));
 	class_destroy(my_class);
 	cdev_del(&cdev);
 	unregister_chrdev_region(first_dev, mq_count);
-	pr_info("device unloaded successfuly\n");
+	for (i = 0; i < mq_count; i++)
+		m_queues_dtor(m_queues+i);
+	kfree(m_queues);
+	pr_info(KBUILD_MODNAME " unloaded successfully\n");
 }
 
 module_init(mq_init);
